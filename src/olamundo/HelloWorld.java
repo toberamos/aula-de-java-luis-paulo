d93397e2e9b8af80fb1a8b83391fddebba3fbf1a
36115152016.3 <36115152016.3@36DE194582.senacrjedu.local>
2017-11-16 11:59:07 -0200

    Primeiro commit

---
+++ src/olamundo/HelloWorld.java
@@ -0,0 +1,10 @@
+package olamundo;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+//		System.out.println("olá Mundo Java 2016.3");
+		JOptionPane.showMessageDialog(null, "Olá Mundo!");
+	}
+}
